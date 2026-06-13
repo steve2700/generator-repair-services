b5bc4ece948c213ed9b9106ab1fcf09a413c246a
Stewart Nyaruwata <112034196+steve2700@users.noreply.github.com>
2026-06-13 22:59:37 +0200

    Update robots.ts

--- app/robots.ts
+++ app/robots.ts
@@ -9,6 +9,6 @@ export default function robots(): MetadataRoute.Robots {
         disallow: ['/api/', '/_next/', '/admin/'],
       },
     ],
-    sitemap: 'https://generatorrepairservices.co.za/sitemap.xml',
+    sitemap: 'https://www.generatorrepairservices.co.za/sitemap.xml',
   }
 }
